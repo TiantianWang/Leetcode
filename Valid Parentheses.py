87cbd6b379f60483a976ec3693799c3e54750083
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-16 14:45:44 -0800

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,28 +1,75 @@
 """
 Description
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+An input string is valid if:
 
-Example:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
 
-Given nums = [2, 7, 11, 15], target = 9,
+Example 1:
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
 """
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def isValid(self, s):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        :type s: str
+        :rtype: bool
         """
-        a ={}
-        for i, num in enumerate(nums):
-            if target-num in a:
-                return [a[target - num], i]
+        '''
+        temp = []
+        for i in len(s):
+            if i == '(' or '{' or '[':
+                temp.append(i)
+            elif i == ')' or '}' or ']':
+                if temp[-1] == '(' and i == ')' or temp[-1] == '{' and i == '}'
+        ''' 
+        # The stack to keep track of opening brackets.
+        stack = []
+
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
+        t=0
+        # For every bracket in the expression.
+        for char in s:
+            # If the character is an closing bracket
+            if char in mapping:
+                print t
+                t=t+1
+                # Pop the topmost element from the stack, if it is non empty
+                # Otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
+
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if mapping[char] != top_element:
+                    return False
             else:
-                a[num] = i
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
+
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return not stack
