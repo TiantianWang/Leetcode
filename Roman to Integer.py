68f5b3fdf37f1d70fd1867e3289742460616ee5c
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-16 11:00:11 -0800

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,28 +1,76 @@
 """
 Description
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-Example:
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
 
-Given nums = [2, 7, 11, 15], target = 9,
+Example 1:
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+Example 3:
+
+Input: "IX"
+Output: 9
+Example 4:
+
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 5:
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def romanToInt(self, s):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        :type s: str
+        :rtype: int
         """
-        a ={}
-        for i, num in enumerate(nums):
-            if target-num in a:
-                return [a[target - num], i]
+        symbols_integer = {'I':1, 'V':5, 'X':10, 'L':50,
+                           'C':100, 'D':500, 'M':1000,
+                           'IV':4, 'IX':9, 'XL':40, 'XC':90,
+                           'CD':400, 'CM':900}
+        length = len(s)
+        integer = 0
+        isPass = False
+
+        for i in range(length):
+            if isPass:
+                isPass = False
+                continue
+
+            if s[i:i+2] not in symbols_integer:
+                integer = integer + symbols_integer[s[i]]
+                isPass = False
+                continue
+
             else:
-                a[num] = i
+                integer = integer + symbols_integer[s[i:i+2]]
+                isPass = True
+                continue
+
+        return integer
+      
