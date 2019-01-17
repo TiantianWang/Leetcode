2fd768c637e06e0d71183dff27e643929c31be85
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-16 22:09:15 -0800

    Update 26 Remove Duplicates from Sorted Array.py

--- 26 Remove Duplicates from Sorted Array.py
+++ 26 Remove Duplicates from Sorted Array.py
@@ -1,28 +1,38 @@
 """
 Description
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-Example:
+Example 1:
 
-Given nums = [2, 7, 11, 15], target = 9,
+Given nums = [1,1,2],
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
 """
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        a ={}
-        for i, num in enumerate(nums):
-            if target-num in a:
-                return [a[target - num], i]
-            else:
-                a[num] = i
+        :rtype: int
+        """      
+        if not nums:
+            return 0
+        index = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[index]:
+                index = index + 1
+                nums[index] = nums[i]
+
+        return index + 1
