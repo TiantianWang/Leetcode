660aff3d0a77458e145da863f782ed81ece7076c
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 11:12:28 -0800

    Update 53 Maximum Subarray.py

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -1,28 +1,27 @@
 """
 Description
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 Example:
 
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
 """
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def maxSubArray(self, nums):
         """
         :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        :rtype: int
         """
-        a ={}
-        for i, num in enumerate(nums):
-            if target-num in a:
-                return [a[target - num], i]
-            else:
-                a[num] = i
+        max_num = - 2 ** 31
+        this = 0
+        for i in range(len(nums)):
+            
+            if this < 0:
+                this = 0
+            this = this + nums[i]
+            max_num = max(this, max_num)
+        return max_num
