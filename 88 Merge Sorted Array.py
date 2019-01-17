1592fe51f6a77665d957bc7abe22141f2f7b80aa
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 15:51:46 -0800

    Update 88 Merge Sorted Array.py

--- 88 Merge Sorted Array.py
+++ 88 Merge Sorted Array.py
@@ -1,27 +1,38 @@
 """
 Description
 
-Given two binary strings, return their sum (also a binary string).
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-The input strings are both non-empty and contains only characters 1 or 0.
+Note:
 
-Example 1:
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
 
-Input: a = "11", b = "1"
-Output: "100"
-Example 2:
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
 
-Input: a = "1010", b = "1011"
-Output: "10101"
+Output: [1,2,2,3,5,6]
+
 """
 
 class Solution(object):
-    def addBinary(self, a, b):
+    def merge(self, nums1, m, nums2, n):
         """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        a_b = int(a, 2) + int(b, 2)
-        a_b = str(bin(a_b))
-        return a_b[2:]
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """        
+        
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
