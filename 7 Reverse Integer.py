2f2db18c472b66d6d455c0d6b48188fa27e74b1d
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 13:25:45 -0800

    Update 7 Reverse Integer.py

--- 7 Reverse Integer.py
+++ 7 Reverse Integer.py
@@ -1,16 +1,21 @@
 """
 Description
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Given a 32-bit signed integer, reverse digits of an integer.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example 1:
 
-Example:
+Input: 123
+Output: 321
+Example 2:
 
-Given nums = [2, 7, 11, 15], target = 9,
+Input: -123
+Output: -321
+Example 3:
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Input: 120
+Output: 21
+
 """
 
 class Solution:
