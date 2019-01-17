21a28671422fbb4645177727d21d2a8451ced706
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 13:25:03 -0800

    Update 67 Add Binary.py

--- 67 Add Binary.py
+++ 67 Add Binary.py
@@ -1,73 +1,27 @@
 """
 Description
 
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Given two binary strings, return their sum (also a binary string).
 
-An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Note that an empty string is also considered valid.
+The input strings are both non-empty and contains only characters 1 or 0.
 
 Example 1:
 
-Input: "()"
-Output: true
+Input: a = "11", b = "1"
+Output: "100"
 Example 2:
 
-Input: "()[]{}"
-Output: true
-Example 3:
-
-Input: "(]"
-Output: false
-Example 4:
-
-Input: "([)]"
-Output: false
-Example 5:
-
-Input: "{[]}"
-Output: true
+Input: a = "1010", b = "1011"
+Output: "10101"
 """
 
 class Solution(object):
-    def isValid(self, s):
+    def addBinary(self, a, b):
         """
-        :type s: str
-        :rtype: bool
+        :type a: str
+        :type b: str
+        :rtype: str
         """
-        '''
-        temp = []
-        for i in len(s):
-            if i == '(' or '{' or '[':
-                temp.append(i)
-            elif i == ')' or '}' or ']':
-                if temp[-1] == '(' and i == ')' or temp[-1] == '{' and i == '}'
-        ''' 
-        # The stack to keep track of opening brackets.
-        stack = []
-
-        # Hash map for keeping track of mappings. This keeps the code very clean.
-        # Also makes adding more types of parenthesis easier
-        mapping = {")": "(", "}": "{", "]": "["}
-        # For every bracket in the expression.
-        for char in s:
-            # If the character is an closing bracket
-            if char in mapping:
-               
-                # Pop the topmost element from the stack, if it is non empty
-                # Otherwise assign a dummy value of '#' to the top_element variable
-                top_element = stack.pop() if stack else '$' #if delete else, when s = ']', there will be error.
-
-                # The mapping for the opening bracket in our hash and the top
-                # element of the stack don't match, return False
-                if mapping[char] != top_element:
-                    return False
-            else:
-                # We have an opening bracket, simply push it onto the stack.
-                stack.append(char)
-
-        # In the end, if the stack is empty, then we have a valid expression.
-        # The stack won't be empty for cases like ((()
-        return not stack
+        a_b = int(a, 2) + int(b, 2)
+        a_b = str(bin(a_b))
+        return a_b[2:]
