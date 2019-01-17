386fe22ceba30a27dfee092e0a612bfa8bee230a
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 09:29:35 -0800

    Update 27 Remove Element.py

--- 27 Remove Element.py
+++ 27 Remove Element.py
@@ -1,28 +1,40 @@
 """
 Description
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-Example:
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
-Given nums = [2, 7, 11, 15], target = 9,
+Example 1:
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
 """
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def removeElement(self, nums, val):
         """
         :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        :type val: int
+        :rtype: int
         """
-        a ={}
-        for i, num in enumerate(nums):
-            if target-num in a:
-                return [a[target - num], i]
-            else:
-                a[num] = i
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count = count + 1
+        return count
