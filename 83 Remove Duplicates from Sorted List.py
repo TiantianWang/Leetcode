3c0e547db4b766f18386cae766072708a5de638a
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 15:40:15 -0800

    Update 83 Remove Duplicates from Sorted List.py

--- 83 Remove Duplicates from Sorted List.py
+++ 83 Remove Duplicates from Sorted List.py
@@ -1,27 +1,37 @@
 """
 Description
 
-Given two binary strings, return their sum (also a binary string).
-
-The input strings are both non-empty and contains only characters 1 or 0.
+Given a sorted linked list, delete all duplicates such that each element appear only once.
 
 Example 1:
 
-Input: a = "11", b = "1"
-Output: "100"
+Input: 1->1->2
+Output: 1->2
 Example 2:
 
-Input: a = "1010", b = "1011"
-Output: "10101"
+Input: 1->1->2->3->3
+Output: 1->2->3
+
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
-    def addBinary(self, a, b):
+    def deleteDuplicates(self, head):
         """
-        :type a: str
-        :type b: str
-        :rtype: str
+        :type head: ListNode
+        :rtype: ListNode
         """
-        a_b = int(a, 2) + int(b, 2)
-        a_b = str(bin(a_b))
-        return a_b[2:]
+        if not head:
+            return head
+        dummy = head
+        while dummy.next:
+            if dummy.val == dummy.next.val:
+                dummy.next = dummy.next.next
+            else:
+                dummy = dummy.next
+        return head
