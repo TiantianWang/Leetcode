19f8b0f294786d3225f4e699089b1c60dc43affd
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 21:06:47 -0800

    Add files via upload

---
+++ 125 Valid Palindrome.py
@@ -0,0 +1,27 @@
+"""
+Description
+
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+"""
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a_b = int(a, 2) + int(b, 2)
+        a_b = str(bin(a_b))
+        return a_b[2:]
