33e87fedaa235965833702a1430845d11b59f73d
TiantianWang <tiantianwang.ice@gmail.com>
2019-01-17 23:08:21 -0800

    Update 189 Rotate Array.py

--- 189 Rotate Array.py
+++ 189 Rotate Array.py
@@ -1,27 +1,32 @@
 """
 Description
 
-Given two binary strings, return their sum (also a binary string).
-
-The input strings are both non-empty and contains only characters 1 or 0.
+Given an array, rotate the array to the right by k steps, where k is non-negative.
 
 Example 1:
 
-Input: a = "11", b = "1"
-Output: "100"
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
 Example 2:
 
-Input: a = "1010", b = "1011"
-Output: "10101"
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
 """
 
 class Solution(object):
-    def addBinary(self, a, b):
+    def rotate(self, nums, k):
         """
-        :type a: str
-        :type b: str
-        :rtype: str
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
         """
-        a_b = int(a, 2) + int(b, 2)
-        a_b = str(bin(a_b))
-        return a_b[2:]
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]
